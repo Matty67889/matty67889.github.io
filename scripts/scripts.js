2a011ad519bfc5c6d4b42089a449a5946f67d455
Matthew Mentis-Cort <matty67889@gmail.com>
2024-10-30 16:19:16 -0400

    Fix navbar bugs, improve design
    - Fix bug where if the screen is
    narrow and the user resizes the
    window to wide display size, then
    resizes it to narrow again, the
    navbar links stay open
    - Move navbar display functionality
    to be Javascript only
    - Aligned navbar links to center
    on wide displays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,34 @@
 const navLinksCont = document.getElementById("nav-links-container");
 
-function toggleNavVisibility() {
+/*
+ * using DOMContentLoaded event because I don't want links to hide
+ * to happen after all content is loadded, but I want
+ * them to hide after the page is loaded, so I need an
+ * equivalent of jQuery's document.ready()
+ * Sources:
+ * https://builtin.com/articles/document-ready-javascript
+ */
+// toggles nav link visibility when document loaded for mobile devices
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth < 768) {
+    navLinksCont.classList.add("hidden");
+  } else {
+    navLinksCont.classList.remove("hidden");
+  }
+})
+
+// toggles nav link visibility on window resize
+// (so links don't stay open when opening links
+// on mobile then going from desktop to mobile)
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    navLinksCont.classList.add("hidden");
+  } else {
+    navLinksCont.classList.remove("hidden");
+  }
+})
+
+function toggleLinksVisibility() {
   if (navLinksCont.classList.contains("hidden")) {
     navLinksCont.classList.remove("hidden");
   } else {
